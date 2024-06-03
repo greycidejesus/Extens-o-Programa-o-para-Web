9a0515887bd3ac172c4b9b8e40afdb6242a06079
Greyciane <107276442+greycidejesus@users.noreply.github.com>
2024-06-03 20:25:29 -0300

    Update calculadoraRoutes.js

--- app_calc /src/routes/calculadoraRoutes.js
+++ app_calc /src/routes/calculadoraRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const calculadoraControlador = require('../controlador/calculadoraControlador');
+const path = require('path');
 
 router.get('/', (req, res) => {
-  res.sendFile('index.html', { root: 'src/views' });
+    res.sendFile(path.join(__dirname, '../views', 'index.html'));
 });
 
 router.post('/calcular', calculadoraControlador.calcular);
 
 module.exports = router;
+
 
