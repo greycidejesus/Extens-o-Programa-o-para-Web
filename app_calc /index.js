7898fe8412577e602d575b273f1b52d7e38f2b99
Greyciane <107276442+greycidejesus@users.noreply.github.com>
2024-06-03 19:18:26 -0300

    Update index.js

--- app_calc /index.js
+++ app_calc /index.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+const calculadoraRoutes = require('./src/routes/calculadoraRoutes');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/', calculadoraRoutes);
+
+app.listen(port, () => {
+  console.log(`App running at http://localhost:${port}`);
+});
